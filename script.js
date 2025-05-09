881452991c65adefb84bade0e5b665bae4d6d1ae
AleexStr <135070967+AleexStr@users.noreply.github.com>
2025-05-09 17:34:06 -0600

    cambios

--- script.js
+++ script.js
@@ -33,3 +33,33 @@ document.addEventListener("click", (e) => {
         }
     }
 });
+
+const flores = [
+  'images/gerbera.png',
+  'images/tulipan.png',
+  'images/rosa.png',
+  'images/girasol.png',
+  'images/lirio.png',
+  'images/orquidea.png',
+  'images/gerberas.png'
+];
+
+function lanzarFlor() {
+  const flor = document.createElement('img');
+  flor.src = flores[Math.floor(Math.random() * flores.length)];
+  flor.classList.add('flor-cascada');
+
+  const tamaño = Math.random() * 30 + 60; // entre 30 y 60px
+  flor.style.width = `${tamaño}px`;
+
+  flor.style.left = `${Math.random() * 100}%`;
+  flor.style.animationDuration = `${5 + Math.random() * 4}s`; // 6 a 10s
+
+  document.body.appendChild(flor);
+
+  flor.addEventListener('animationend', () => flor.remove());
+}
+
+setInterval(() => {
+  lanzarFlor();
+}, 500); // lanza una flor cada 0.5s
